a17108a5e70a713d5e49e083f8fb226ac2ae233d
Mike <grabbou@gmail.com>
2024-03-26 12:46:10 +0100

    chore: move files around

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -5,10 +5,10 @@ import '@tamagui/core/reset.css'
 import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
 
-import { Provider } from 'app-demo/Provider'
 import Head from 'next/head'
 import React from 'react'
 import type { SolitoAppProps } from 'solito'
+import { Provider } from 'src/Provider'
 
 if (process.env.NODE_ENV === 'production') {
   require('../public/tamagui.css')
