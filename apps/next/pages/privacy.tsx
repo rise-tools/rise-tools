21dbc3f5b5c959dde7b9c401a7672ebe23caad9f
Eric Vicenti <eric@vicenti.net>
2024-03-26 14:31:09 -0700

    Stub out new pages

---
+++ apps/next/pages/privacy.tsx
@@ -0,0 +1,5 @@
+import { SizableText } from '@react-native-templates/demo-ui'
+
+export default function PrivacyPage() {
+  return <SizableText>Rise Privacy Policy</SizableText>
+}
---
+++ apps/next/pages/support.tsx
@@ -0,0 +1,5 @@
+import { SizableText } from '@react-native-templates/demo-ui'
+
+export default function SupportPage() {
+  return <SizableText>Rise Support</SizableText>
+}
