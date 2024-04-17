4cf1403f8ed599f0fdab065d341b5d4ea14513c8
Mike <grabbou@gmail.com>
2024-04-17 21:32:56 +0200

    feat: add CI for tests and configure Jest (#9)
    * feat: let jest always read from source
    
    * feat: add simple gh action
    
    * chore: add jest setup file and unify testing environment to jsdom
    
    * chore: add comment on workaround

--- packages/ui-tamagui/src/__tests__/index.test.tsx
+++ packages/ui-tamagui/src/__tests__/index.test.tsx
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
 import { BaseTemplate } from '@react-native-templates/core'
 import { config } from '@tamagui/config/v3'
 import { render } from '@testing-library/react'
@@ -40,21 +36,32 @@ it('should render a Tamagui component', () => {
 
   expect(element.asFragment()).toMatchInlineSnapshot(`
     <DocumentFragment>
-      <view
-        style="flex-direction: row;"
+      <span
+        class="t_light is_inversed _dsp_contents"
       >
-        <text
-          accessibilityrole="header"
-          style="color: rgb(23, 23, 23); text-transform: none; font-family: InterBold; letter-spacing: -1.5px; font-size: 44px; line-height: 53; margin: 0px 0px 0px 0px;"
+        <span
+          class=" t_light _dsp_contents is_Theme"
         >
-          Hello
-        </text>
-        <text
-          style="color: rgb(23, 23, 23); font-family: Inter; letter-spacing: 0; font-size: 14px; line-height: 23;"
-        >
-          Welcome to Tamagui!
-        </text>
-      </view>
+          <div
+            class="_dsp-flex _ai-stretch _fd-row _fb-auto _bxs-border-box _pos-relative _mih-0px _miw-0px _fs-0"
+            data-testid="root"
+          >
+            <h1
+              class="is_H1 font_heading _col-675002279 _tt-1440318557 _ff-299667014 _fow-1366436877 _ls-905095908 _fos-1477259397 _lh-1677663454 _dsp-inline _bxs-border-box _ww-break-word _whiteSpace-normal _mt-0px _mr-0px _mb-0px _ml-0px _ussel-auto"
+              data-testid="root.children[0]"
+              role="heading"
+            >
+              Hello
+            </h1>
+            <p
+              class="is_Paragraph font_body _col-675002279 _ff-299667014 _fow-233016140 _ls-167744059 _fos-229441220 _lh-222976573 _dsp-inline _bxs-border-box _ww-break-word _whiteSpace-normal _mt-0px _mr-0px _mb-0px _ml-0px _ussel-auto"
+              data-testid="root.children[1]"
+            >
+              Welcome to Tamagui!
+            </p>
+          </div>
+        </span>
+      </span>
     </DocumentFragment>
   `)
 })
