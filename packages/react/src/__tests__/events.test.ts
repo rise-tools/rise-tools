f7f1cc2b47c4a2eb0e121a8b3bf3ed9cf03e4a4d
Mike <grabbou@gmail.com>
2024-04-25 15:34:37 +0200

    feat: add tests

---
+++ packages/react/src/__tests__/events.test.ts
@@ -0,0 +1,28 @@
+import { getAllEventHandlers, handler, ServerDataState } from '../events'
+
+it('should return all event handlers from the object', () => {
+  const eventHandler = handler(jest.fn())
+
+  const state: ServerDataState = {
+    $: 'component',
+    component: 'View',
+    props: {
+      // this is not an event handler and should be ignored
+      onPress: {
+        $: 'event',
+      },
+    },
+    children: {
+      $: 'component',
+      component: 'View',
+      props: {
+        onPress: eventHandler,
+      },
+    },
+  }
+
+  const res = getAllEventHandlers(state)
+
+  expect(Object.keys(res).length).toBe(1)
+  expect(getAllEventHandlers(state)[eventHandler.key]).toBeDefined()
+})
