5e9fcdf414ed11d0e5f3dd7bdc75d641058a96de
Mike <grabbou@gmail.com>
2024-04-28 21:13:05 +0200

    add tests

--- packages/ws-client/src/__tests__/data-source.test.ts
+++ packages/ws-client/src/__tests__/data-source.test.ts
@@ -1,1 +1,74 @@
-it.todo('write a test')
+import { TemplateEvent } from '@final-ui/react'
+import WS from 'jest-websocket-mock'
+
+import { createWSDataSource, EventResponseWebsocketMessage } from '../data-source'
+
+let ws: WS
+
+beforeEach(() => {
+  ws = new WS('ws://localhost:8080')
+})
+afterEach(() => {
+  WS.clean()
+})
+
+it('should resolve a promise once response comes in', async () => {
+  const dataSource = createWSDataSource('ws://localhost:8080')
+  await ws.connected
+
+  const event: TemplateEvent = {
+    target: {
+      key: 'key',
+      path: 'mainStore',
+      component: 'View',
+    },
+    dataState: {
+      $: 'event',
+      key: 'key',
+    },
+    payload: null,
+  }
+  const response: EventResponseWebsocketMessage = {
+    $: 'evt-res',
+    key: 'key',
+    ok: true,
+    val: 'response',
+  }
+
+  const promise = dataSource.sendEvent(event)
+  ws.send(JSON.stringify(response))
+
+  expect(await promise).toEqual('response')
+})
+
+it('should timeout if response comes too late', async () => {
+  const dataSource = createWSDataSource('ws://localhost:8080')
+  await ws.connected
+
+  const event: TemplateEvent = {
+    target: {
+      key: 'key',
+      path: 'mainStore',
+      component: 'View',
+    },
+    dataState: {
+      $: 'event',
+      key: 'key',
+      timeout: 1000,
+    },
+    payload: null,
+  }
+  const response: EventResponseWebsocketMessage = {
+    $: 'evt-res',
+    key: 'key',
+    ok: true,
+    val: 'response',
+  }
+
+  const promise = dataSource.sendEvent(event)
+  setTimeout(() => {
+    ws.send(JSON.stringify(response))
+  }, 2000)
+
+  expect(promise).rejects.toMatch('timeout')
+})
