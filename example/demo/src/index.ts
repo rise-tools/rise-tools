6eb3120eaf08efb1d1564fa6e361ce23f9deaaab
Mike <grabbou@gmail.com>
2024-04-29 20:54:14 +0200

    tweaks

--- example/demo/src/index.ts
+++ example/demo/src/index.ts
@@ -4,36 +4,36 @@ import { getInventoryExample } from './inventory/ui'
 import { UIContext } from './types'
 
 export default {
-	async fetch(request: Request): Promise<Response> {
-		const upgrade = request.headers.get('Upgrade')
-		if (!upgrade || upgrade !== 'websocket') {
-			return new Response('Expected Upgrade: websocket', { status: 426 })
-		}
+  async fetch(request: Request): Promise<Response> {
+    const upgrade = request.headers.get('Upgrade')
+    if (!upgrade || upgrade !== 'websocket') {
+      return new Response('Expected Upgrade: websocket', { status: 426 })
+    }
 
-		const webSocketPair = new WebSocketPair()
-		const [client, server] = Object.values(webSocketPair) as [WebSocket, WebSocket]
+    const webSocketPair = new WebSocketPair()
+    const [client, server] = Object.values(webSocketPair) as [WebSocket, WebSocket]
 
-		// Accepts the WebSocket connection and begins terminating requests
-		// for the WebSocket on Cloudflare’s global network.
-		server.accept()
+    // Accepts the WebSocket connection and begins terminating requests
+    // for the WebSocket on Cloudflare’s global network.
+    server.accept()
 
-		const dataSource = createWSDataSource()
+    const dataSource = createWSDataSource()
 
-		dataSource.handleWSConnection(server)
+    dataSource.handleWSConnection(server)
 
-		const ctx: UIContext = {
-			update: (key, updater) => {
-				dataSource.update(key, updater(dataSource.get(key)))
-			},
-		}
+    const ctx: UIContext = {
+      update: (key, updater) => {
+        dataSource.update(key, updater(dataSource.get(key)))
+      },
+    }
 
-		for (const [key, value] of Object.entries(getInventoryExample(ctx))) {
-			dataSource.update(key, value)
-		}
+    for (const [key, value] of Object.entries(getInventoryExample(ctx))) {
+      dataSource.update(key, value)
+    }
 
-		return new Response(null, {
-			status: 101,
-			webSocket: client,
-		})
-	},
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    })
+  },
 }
