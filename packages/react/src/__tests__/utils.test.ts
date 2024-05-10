6ef1672b8e1d6332e2fd54ca92ccb26bfe8cc6b8
Mike <grabbou@gmail.com>
2024-05-09 22:31:31 -0400

    add

--- packages/react/src/__tests__/utils.test.ts
+++ packages/react/src/__tests__/utils.test.ts
@@ -4,6 +4,7 @@ it('should return true if items are of the same type', () => {
   expect(assertEvery([], (item) => typeof item === 'object')).toBe(true)
   expect(assertEvery([1, 2, 3], (item: number) => typeof item === 'number')).toBe(true)
   expect(assertEvery([{ $: 'event' }, { $: 'event' }], (item) => item.$ === 'event')).toBe(true)
+  expect(assertEvery([{ $: 'event' }, { $: 'event' }], (item) => item.$ === 'action')).toBe(true)
 })
 
 it('should throw if items are different', () => {
