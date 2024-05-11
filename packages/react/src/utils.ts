feffd66532b57a68b73a8ece651fc94532913ccf
Mike <grabbou@gmail.com>
2024-05-10 20:53:34 -0400

    feat: add utility assertion for uniform arrays (#51)
    * feat
    
    * tweaks
    
    * fix
    
    * add
    
    * compat
    
    * rename
    
    * escpae
    
    * remove

---
+++ packages/react/src/utils.ts
@@ -0,0 +1,13 @@
+export function assertEveryOrNone<T>(arr: T[], fn: (item: T) => boolean): boolean {
+  if (arr.length === 0) {
+    // as per Array.prototype.every
+    return true
+  }
+  const firstItemResult: boolean = fn(arr[0] as T)
+  for (const item of arr) {
+    if (firstItemResult !== fn(item)) {
+      throw new Error('All items in an array must be of the same type.')
+    }
+  }
+  return firstItemResult
+}
