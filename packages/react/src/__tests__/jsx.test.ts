22ef1ca59fecccfa29e67dc8e5b2213f91cc2122
Mike <grabbou@gmail.com>
2024-05-15 22:46:13 -0700

    fix

---
+++ packages/react/src/__tests__/jsx.test.ts
@@ -0,0 +1,28 @@
+import { action } from '../events'
+import { createComponentDefinition, jsx } from '../jsx'
+import { isEventDataState } from '../template'
+
+const DummyComponent = createComponentDefinition('DummyComponent')
+
+it('should turn function into event', () => {
+  const el = jsx(DummyComponent, {
+    onPress: jest.fn(),
+    children: null,
+  })
+  expect(isEventDataState(el.props!.onPress)).toBe(true)
+})
+
+it('should turn action into event', () => {
+  const el = jsx(DummyComponent, {
+    onPress: action('my-action'),
+    children: null,
+  })
+  expect(isEventDataState(el.props!.onPress)).toBe(true)
+})
+
+it.skip('should compose higher-order functions together', () => {
+  // todo
+})
+it.skip('should create ComponentDataState', () => {
+  // todo
+})
