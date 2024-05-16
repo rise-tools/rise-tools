4f0302a689ff899438707addc1de9dd2bafffa76
Mike <grabbou@gmail.com>
2024-05-16 13:58:39 -0700

    remove cache from the server

--- packages/http-server/src/data-source.ts
+++ packages/http-server/src/data-source.ts
@@ -4,16 +4,12 @@ type Initializer = ServerDataState | UI | (() => Promise<ServerDataState | UI>)
 
 export function createHTTPDataSource() {
   const values = new Map<string, Initializer>()
-  const cache = new Map<string, ServerDataState>()
 
   function update(key: string, value: Initializer) {
     values.set(key, value)
   }
 
   async function get(path: string) {
-    if (cache.has(path)) {
-      return cache.get(path)
-    }
     let value = values.get(path)
     if (typeof value === 'function') {
       value = await value()
@@ -23,7 +19,6 @@ export function createHTTPDataSource() {
         'Rise JSX not configured. You must set "jsx" to "react-jsx" and "jsxImportSource" to "@final-ui/react" in your tsconfig.json.'
       )
     }
-    cache.set(path, value)
     return value
   }
 
