9ba7f8f3395b779c029f04cbb38e56c61e63aa21
Mike <grabbou@gmail.com>
2024-06-01 16:04:12 +0200

    chore: remove cloudflare from ws

--- example/demo/src/index.ts
+++ example/demo/src/index.ts
@@ -1,40 +1,14 @@
-import { createWSServerDataSource } from '@final-ui/ws-server'
+import { createWSServer } from '@final-ui/ws-server'
 
 import { InventoryExample } from './inventory/ui'
 import { UIContext } from './types'
 
-function setupDataSource() {
-  const dataSource = createWSServerDataSource()
-
-  const ctx: UIContext = {
-    update: async (key, updater) => {
-      dataSource.update(key, updater(await dataSource.get(key)))
-    },
-  }
-  for (const [key, value] of Object.entries(InventoryExample(ctx))) {
-    dataSource.update(key, value)
-  }
-
-  return dataSource
-}
-
-export default {
-  async fetch(request: Request): Promise<Response> {
-    const upgrade = request.headers.get('Upgrade')
-    if (!upgrade || upgrade !== 'websocket') {
-      return new Response('Expected Upgrade: websocket', { status: 426 })
-    }
-
-    const webSocketPair = new WebSocketPair()
-    const [client, server] = Object.values(webSocketPair) as [WebSocket, WebSocket]
-
-    server.accept()
-
-    setupDataSource().handleWSConnection(server)
-
-    return new Response(null, {
-      status: 101,
-      webSocket: client,
-    })
+const dataSource = createWSServer(Number(process.env.PORT || '3005'))
+const ctx: UIContext = {
+  update: async (key, updater) => {
+    dataSource.update(key, updater(await dataSource.get(key)))
   },
+}
+for (const [key, value] of Object.entries(InventoryExample(ctx))) {
+  dataSource.update(key, value)
 }
