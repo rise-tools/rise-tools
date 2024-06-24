73ea3d3551a0e8027897e112426b5b0f4a08c66b
Mike <grabbou@gmail.com>
2024-06-24 16:38:44 +0200

    complete rename

---
+++ packages/kit-tamagui/src/server.ts
@@ -0,0 +1,323 @@
+import { createComponentDefinition } from '@rise-tools/react'
+import * as t from 'tamagui'
+import { LinearGradient as TLinearGradient } from 'tamagui/linear-gradient'
+
+export const Adapt = createComponentDefinition<typeof t.Adapt>('rise-tools/kit-tamagui/Adapt')
+export const AdaptContents = createComponentDefinition<typeof t.Adapt.Contents>(
+  'rise-tools/kit-tamagui/AdaptContents'
+)
+
+export const View = createComponentDefinition<typeof t.View>('rise-tools/kit-tamagui/View')
+export const XStack = createComponentDefinition<typeof t.XStack>('rise-tools/kit-tamagui/XStack')
+export const YStack = createComponentDefinition<typeof t.YStack>('rise-tools/kit-tamagui/YStack')
+export const ZStack = createComponentDefinition<typeof t.ZStack>('rise-tools/kit-tamagui/ZStack')
+export const SizableStack = createComponentDefinition<typeof t.SizableStack>(
+  'rise-tools/kit-tamagui/SizableStack'
+)
+
+export const H1 = createComponentDefinition<typeof t.H1>('rise-tools/kit-tamagui/H1')
+export const H2 = createComponentDefinition<typeof t.H2>('rise-tools/kit-tamagui/H2')
+export const H3 = createComponentDefinition<typeof t.H3>('rise-tools/kit-tamagui/H3')
+export const H4 = createComponentDefinition<typeof t.H4>('rise-tools/kit-tamagui/H4')
+export const H5 = createComponentDefinition<typeof t.H5>('rise-tools/kit-tamagui/H5')
+export const H6 = createComponentDefinition<typeof t.H6>('rise-tools/kit-tamagui/H6')
+export const Heading = createComponentDefinition<typeof t.Heading>('rise-tools/kit-tamagui/Heading')
+
+export const Text = createComponentDefinition<typeof t.Text>('rise-tools/kit-tamagui/Text')
+export const Paragraph = createComponentDefinition<typeof t.Paragraph>(
+  'rise-tools/kit-tamagui/Paragraph'
+)
+export const SizableText = createComponentDefinition<typeof t.SizableText>(
+  'rise-tools/kit-tamagui/SizableText'
+)
+
+export const Button = createComponentDefinition<typeof t.Button>('rise-tools/kit-tamagui/Button')
+export const Checkbox = createComponentDefinition<typeof t.Checkbox>(
+  'rise-tools/kit-tamagui/Checkbox'
+)
+export const Form = createComponentDefinition<typeof t.Form>('rise-tools/kit-tamagui/Form')
+export const FormTrigger = createComponentDefinition<typeof t.FormTrigger>(
+  'rise-tools/kit-tamagui/FormTrigger'
+)
+export const Input = createComponentDefinition<typeof t.Input>('rise-tools/kit-tamagui/Input')
+export const TextArea = createComponentDefinition<typeof t.TextArea>(
+  'rise-tools/kit-tamagui/TextArea'
+)
+
+export const Label = createComponentDefinition<typeof t.Label>('rise-tools/kit-tamagui/Label')
+
+export const Progress = createComponentDefinition<typeof t.Progress>(
+  'rise-tools/kit-tamagui/Progress'
+)
+export const ProgressIndicator = createComponentDefinition<typeof t.ProgressIndicator>(
+  'rise-tools/kit-tamagui/ProgressIndicator'
+)
+export const RadioGroup = createComponentDefinition<typeof t.RadioGroup>(
+  'rise-tools/kit-tamagui/RadioGroup'
+)
+export const RadioGroupItem = createComponentDefinition<typeof t.RadioGroup.Item>(
+  'rise-tools/kit-tamagui/RadioGroupItem'
+)
+export const RadioGroupIndicator = createComponentDefinition<typeof t.RadioGroup.Indicator>(
+  'rise-tools/kit-tamagui/RadioGroupIndicator'
+)
+
+export const Select = createComponentDefinition<typeof t.Select>('rise-tools/kit-tamagui/Select')
+export const SelectTrigger = createComponentDefinition<typeof t.Select.Trigger>(
+  'rise-tools/kit-tamagui/SelectTrigger'
+)
+export const SelectScrollDownButton = createComponentDefinition<typeof t.Select.ScrollDownButton>(
+  'rise-tools/kit-tamagui/SelectScrollDownButton'
+)
+export const SelectScrollUpButton = createComponentDefinition<typeof t.Select.ScrollUpButton>(
+  'rise-tools/kit-tamagui/SelectScrollUpButton'
+)
+export const SelectViewport = createComponentDefinition<typeof t.Select.Viewport>(
+  'rise-tools/kit-tamagui/SelectViewport'
+)
+export const SelectGroup = createComponentDefinition<typeof t.Select.Group>(
+  'rise-tools/kit-tamagui/SelectGroup'
+)
+export const SelectContent = createComponentDefinition<typeof t.Select.Group>(
+  'rise-tools/kit-tamagui/SelectContent'
+)
+export const SelectLabel = createComponentDefinition<typeof t.Select.Label>(
+  'rise-tools/kit-tamagui/SelectLabel'
+)
+export const SelectItem = createComponentDefinition<typeof t.Select.Item>(
+  'rise-tools/kit-tamagui/SelectItem'
+)
+export const SelectItemIndicator = createComponentDefinition<typeof t.Select.ItemIndicator>(
+  'rise-tools/kit-tamagui/SelectItemIndicator'
+)
+export const SelectItemText = createComponentDefinition<typeof t.Select.ItemText>(
+  'rise-tools/kit-tamagui/SelectItemText'
+)
+export const SelectSheet = createComponentDefinition<typeof t.Select.Sheet>(
+  'rise-tools/kit-tamagui/SelectSheet'
+)
+export const SelectValue = createComponentDefinition<typeof t.Select.Value>(
+  'rise-tools/kit-tamagui/SelectValue'
+)
+
+export const Slider = createComponentDefinition<typeof t.Slider>('rise-tools/kit-tamagui/Slider')
+export const SliderTrack = createComponentDefinition<typeof t.Slider.Track>(
+  'rise-tools/kit-tamagui/SliderTrack'
+)
+export const SliderTrackActive = createComponentDefinition<typeof t.Slider.TrackActive>(
+  'rise-tools/kit-tamagui/SliderTrackActive'
+)
+export const SliderThumb = createComponentDefinition<typeof t.Slider.Thumb>(
+  'rise-tools/kit-tamagui/SliderThumb'
+)
+
+export const Switch = createComponentDefinition<typeof t.Switch>('rise-tools/kit-tamagui/Switch')
+export const SwitchThumb = createComponentDefinition<typeof t.Switch.Thumb>(
+  'rise-tools/kit-tamagui/SwitchThumb'
+)
+export const ToggleGroup = createComponentDefinition<typeof t.ToggleGroup>(
+  'rise-tools/kit-tamagui/ToggleGroup'
+)
+export const ToggleGroupItem = createComponentDefinition<typeof t.ToggleGroup.Item>(
+  'rise-tools/kit-tamagui/ToggleGroupItem'
+)
+
+export const AlertDialog = createComponentDefinition<typeof t.AlertDialog>(
+  'rise-tools/kit-tamagui/AlertDialog'
+)
+export const AlertDialogTitle = createComponentDefinition<typeof t.AlertDialog.Title>(
+  'rise-tools/kit-tamagui/AlertDialogTitle'
+)
+export const AlertDialogContent = createComponentDefinition<typeof t.AlertDialog.Content>(
+  'rise-tools/kit-tamagui/AlertDialogContent'
+)
+export const AlertDialogPortal = createComponentDefinition<typeof t.AlertDialog.Portal>(
+  'rise-tools/kit-tamagui/AlertDialogPortal'
+)
+export const AlertDialogOverlay = createComponentDefinition<typeof t.AlertDialog.Overlay>(
+  'rise-tools/kit-tamagui/AlertDialogOverlay'
+)
+export const AlertDialogAction = createComponentDefinition<typeof t.AlertDialog.Action>(
+  'rise-tools/kit-tamagui/AlertDialogAction'
+)
+export const AlertDialogTrigger = createComponentDefinition<typeof t.AlertDialog.Trigger>(
+  'rise-tools/kit-tamagui/AlertDialogTrigger'
+)
+export const AlertDialogCancel = createComponentDefinition<typeof t.AlertDialog.Cancel>(
+  'rise-tools/kit-tamagui/AlertDialogCancel'
+)
+export const PortalProvider = createComponentDefinition<typeof t.PortalProvider>(
+  'rise-tools/kit-tamagui/PortalProvider'
+)
+
+export const Dialog = createComponentDefinition<typeof t.Dialog>('rise-tools/kit-tamagui/Dialog')
+export const DialogTitle = createComponentDefinition<typeof t.Dialog.Title>(
+  'rise-tools/kit-tamagui/DialogTitle'
+)
+export const DialogContent = createComponentDefinition<typeof t.Dialog.Content>(
+  'rise-tools/kit-tamagui/DialogContent'
+)
+export const DialogPortal = createComponentDefinition<typeof t.Dialog.Portal>(
+  'rise-tools/kit-tamagui/DialogPortal'
+)
+export const DialogOverlay = createComponentDefinition<typeof t.Dialog.Overlay>(
+  'rise-tools/kit-tamagui/DialogOverlay'
+)
+export const DialogClose = createComponentDefinition<typeof t.Dialog.Close>(
+  'rise-tools/kit-tamagui/DialogClose'
+)
+export const DialogSheet = createComponentDefinition<typeof t.Dialog.Sheet>(
+  'rise-tools/kit-tamagui/DialogSheet'
+)
+
+export const Popover = createComponentDefinition<typeof t.Popover>('rise-tools/kit-tamagui/Popover')
+export const PopoverTrigger = createComponentDefinition<typeof t.Popover.Trigger>(
+  'rise-tools/kit-tamagui/PopoverTrigger'
+)
+export const PopoverContent = createComponentDefinition<typeof t.Popover.Content>(
+  'rise-tools/kit-tamagui/PopoverContent'
+)
+export const PopoverAnchor = createComponentDefinition<typeof t.Popover.Anchor>(
+  'rise-tools/kit-tamagui/PopoverAnchor'
+)
+export const PopoverSheet = createComponentDefinition<typeof t.Popover.Sheet>(
+  'rise-tools/kit-tamagui/PopoverSheet'
+)
+export const PopoverScrollView = createComponentDefinition<typeof t.Popover.ScrollView>(
+  'rise-tools/kit-tamagui/PopoverScrollView'
+)
+
+export const Sheet = createComponentDefinition<typeof t.Sheet>('rise-tools/kit-tamagui/Sheet')
+export const SheetOverlay = createComponentDefinition<typeof t.Sheet.Overlay>(
+  'rise-tools/kit-tamagui/SheetOverlay'
+)
+export const SheetFrame = createComponentDefinition<typeof t.Sheet.Frame>(
+  'rise-tools/kit-tamagui/SheetFrame'
+)
+export const SheetHandle = createComponentDefinition<typeof t.Sheet.Handle>(
+  'rise-tools/kit-tamagui/SheetHandle'
+)
+export const SheetScrollView = createComponentDefinition<typeof t.Sheet.ScrollView>(
+  'rise-tools/kit-tamagui/SheetScrollView'
+)
+
+export const Tooltip = createComponentDefinition<typeof t.Tooltip>('rise-tools/kit-tamagui/Tooltip')
+export const TooltipTrigger = createComponentDefinition<typeof t.Tooltip.Trigger>(
+  'rise-tools/kit-tamagui/TooltipTrigger'
+)
+export const TooltipContent = createComponentDefinition<typeof t.Tooltip.Content>(
+  'rise-tools/kit-tamagui/TooltipContent'
+)
+export const TooltipAnchor = createComponentDefinition<typeof t.Tooltip.Anchor>(
+  'rise-tools/kit-tamagui/TooltipAnchor'
+)
+
+export const Accordion = createComponentDefinition<typeof t.Accordion>(
+  'rise-tools/kit-tamagui/Accordion'
+)
+export const AccordionItem = createComponentDefinition<typeof t.Accordion.Item>(
+  'rise-tools/kit-tamagui/AccordionItem'
+)
+export const AccordionHeader = createComponentDefinition<typeof t.Accordion.Header>(
+  'rise-tools/kit-tamagui/AccordionHeader'
+)
+export const AccordionTrigger = createComponentDefinition<typeof t.Accordion.Trigger>(
+  'rise-tools/kit-tamagui/AccordionTrigger'
+)
+export const AccordionContent = createComponentDefinition<typeof t.Accordion.Content>(
+  'rise-tools/kit-tamagui/AccordionContent'
+)
+
+export const Group = createComponentDefinition<typeof t.Group>('rise-tools/kit-tamagui/Group')
+export const GroupItem = createComponentDefinition<typeof t.Group.Item>(
+  'rise-tools/kit-tamagui/GroupItem'
+)
+
+export const Tabs = createComponentDefinition<typeof t.Tabs>('rise-tools/kit-tamagui/Tabs')
+export const TabsList = createComponentDefinition<typeof t.Tabs.List>(
+  'rise-tools/kit-tamagui/TabsList'
+)
+export const TabsTrigger = createComponentDefinition<typeof t.Tabs.Trigger>(
+  'rise-tools/kit-tamagui/TabsTrigger'
+)
+export const TabsContent = createComponentDefinition<typeof t.Tabs.Content>(
+  'rise-tools/kit-tamagui/TabsContent'
+)
+
+export const Avatar = createComponentDefinition<typeof t.Avatar>('rise-tools/kit-tamagui/Avatar')
+export const AvatarImage = createComponentDefinition<typeof t.Avatar.Image>(
+  'rise-tools/kit-tamagui/AvatarImage'
+)
+export const AvatarFallback = createComponentDefinition<typeof t.Avatar.Fallback>(
+  'rise-tools/kit-tamagui/AvatarFallback'
+)
+
+export const Card = createComponentDefinition<typeof t.Card>('rise-tools/kit-tamagui/Card')
+export const CardHeader = createComponentDefinition<typeof t.Card.Header>(
+  'rise-tools/kit-tamagui/CardHeader'
+)
+export const CardFooter = createComponentDefinition<typeof t.Card.Footer>(
+  'rise-tools/kit-tamagui/CardFooter'
+)
+export const CardBackground = createComponentDefinition<typeof t.Card.Background>(
+  'rise-tools/kit-tamagui/CardBackground'
+)
+
+export const Image = createComponentDefinition<typeof t.Image>('rise-tools/kit-tamagui/Image')
+
+export const ListItem = createComponentDefinition<typeof t.ListItem>(
+  'rise-tools/kit-tamagui/ListItem'
+)
+export const ListItemText = createComponentDefinition<typeof t.ListItemText>(
+  'rise-tools/kit-tamagui/ListItemText'
+)
+export const ListItemTitle = createComponentDefinition<typeof t.ListItemTitle>(
+  'rise-tools/kit-tamagui/ListItemTitle'
+)
+export const ListItemSubtitle = createComponentDefinition<typeof t.ListItemSubtitle>(
+  'rise-tools/kit-tamagui/ListItemSubtitle'
+)
+
+export const Separator = createComponentDefinition<typeof t.Separator>(
+  'rise-tools/kit-tamagui/Separator'
+)
+
+export const Square = createComponentDefinition<typeof t.Square>('rise-tools/kit-tamagui/Square')
+
+export const Circle = createComponentDefinition<typeof t.Circle>('rise-tools/kit-tamagui/Circle')
+
+export const Anchor = createComponentDefinition<typeof t.Anchor>('rise-tools/kit-tamagui/Anchor')
+
+export const Section = createComponentDefinition<typeof t.Section>('rise-tools/kit-tamagui/Section')
+
+export const Article = createComponentDefinition<typeof t.Article>('rise-tools/kit-tamagui/Article')
+
+export const Main = createComponentDefinition<typeof t.Main>('rise-tools/kit-tamagui/Main')
+
+export const Header = createComponentDefinition<typeof t.Header>('rise-tools/kit-tamagui/Header')
+
+export const Aside = createComponentDefinition<typeof t.Aside>('rise-tools/kit-tamagui/Aside')
+
+export const Footer = createComponentDefinition<typeof t.Footer>('rise-tools/kit-tamagui/Footer')
+
+export const Nav = createComponentDefinition<typeof t.Nav>('rise-tools/kit-tamagui/Nav')
+
+export const ScrollView = createComponentDefinition<typeof t.ScrollView>(
+  'rise-tools/kit-tamagui/ScrollView'
+)
+
+export const Spinner = createComponentDefinition<typeof t.Spinner>('rise-tools/kit-tamagui/Spinner')
+
+export const Unspaced = createComponentDefinition<typeof t.Unspaced>(
+  'rise-tools/kit-tamagui/Unspaced'
+)
+
+export const VisuallyHidden = createComponentDefinition<typeof t.VisuallyHidden>(
+  'rise-tools/kit-tamagui/VisuallyHidden'
+)
+
+export const LinearGradient = createComponentDefinition<typeof TLinearGradient>(
+  'rise-tools/kit-tamagui/LinearGradient'
+)
+
+export const Theme = createComponentDefinition<typeof t.Theme>('rise-tools/kit-tamagui/Theme')
