34bc9c05cb12fa9223b018c1d1695902088cc0b3
Mike <grabbou@gmail.com>
2024-06-24 16:39:56 +0200

    update

--- packages/kitchen-sink/src/server.ts
+++ packages/kitchen-sink/src/server.ts
@@ -5,14 +5,6 @@ export * from '@rise-tools/kit-haptics/server'
 export * from '@rise-tools/kit-linking/server'
 export * from '@rise-tools/kit-lucide-icons/server'
 export * from '@rise-tools/kit-qrcode/server'
-<<<<<<< feat-pkg-rename
-export * from '@rise-tools/kit-tamagui-toast/server'
-export * from '@rise-tools/kit-tamagui/server'
-
-// tbd: "Circle" collides with Tamagui.Circle. Rename one of them or namespace?
 export * as SVG from '@rise-tools/kit-svg/server'
-=======
-export * as SVG from '@rise-tools/kit-svg/server'
-export * from '@rise-tools/kit-tamagui-toast/server'
 export * from '@rise-tools/kit-tamagui/server'
->>>>>>> main
+export * from '@rise-tools/kit-tamagui-toast/server'
