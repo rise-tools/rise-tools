51ed732e8fe2c7e89811a244c7f4615f602405e2
Mike <grabbou@gmail.com>
2024-07-04 09:05:45 +0530

    feat: better rws config and reconnect devx

--- apps/mobile/src/data-boundary.tsx
+++ apps/mobile/src/data-boundary.tsx
@@ -1,7 +1,7 @@
-import { ModelSource, useStream } from '@rise-tools/react'
+import { createWritableStream, ModelSource, useStream } from '@rise-tools/react'
 import { WebSocketModelSource } from '@rise-tools/ws-client'
 import { AlertCircle } from '@tamagui/lucide-icons'
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
 import { H4, SizableText, Text, YStack } from 'tamagui'
 
 export function DataBoundary({
@@ -11,7 +11,7 @@ export function DataBoundary({
 }: PropsWithChildren<{ modelSource: WebSocketModelSource | ModelSource; path: string }>) {
   const data = useStream(modelSource.get(path))
 
-  if ('state' in modelSource) {
+  if ('ws' in modelSource) {
     return (
       <WebSocketDataBoundary modelSource={modelSource} path={path}>
         {children}
@@ -26,18 +26,59 @@ export function DataBoundary({
   return null
 }
 
+const useStatus = (ws: WebSocketModelSource['ws']) => {
+  const [isConnected, setConnected] = useState<boolean | undefined>(undefined)
+
+  let timeout: NodeJS.Timeout | undefined
+  const setConnectedDelayed = (connected: boolean) => {
+    if (timeout) {
+      return
+    }
+    timeout = setTimeout(() => {
+      setConnected(connected)
+      timeout = undefined
+    }, 1000)
+  }
+
+  useEffect(() => {
+    let hasConnectedAlready = false
+    const onDisconnected = () => {
+      if (hasConnectedAlready) {
+        setConnectedDelayed(false)
+      } else {
+        setConnected(false)
+      }
+    }
+    const onConnected = () => {
+      hasConnectedAlready = true
+      clearTimeout(timeout)
+      setConnected(true)
+    }
+    ws.addEventListener('open', onConnected)
+    ws.addEventListener('close', onDisconnected)
+    ws.addEventListener('error', onDisconnected)
+    return () => {
+      ws.removeEventListener('open', onConnected)
+      ws.removeEventListener('close', onDisconnected)
+      ws.removeEventListener('error', onDisconnected)
+    }
+  }, [ws])
+
+  return isConnected
+}
+
 function WebSocketDataBoundary({
   modelSource,
   path,
   children,
 }: PropsWithChildren<{ modelSource: WebSocketModelSource; path: string }>) {
-  const state = useStream(modelSource.state)
+  const isConnected = useStatus(modelSource.ws)
   const data = useStream(modelSource.get(path))
 
   if (data !== undefined) {
     return (
       <YStack flex={1}>
-        {state.status === 'disconnected' && (
+        {!isConnected && (
           <YStack padding="$3" backgroundColor="$red5">
             <Text textAlign="center" color="$red9">
               You are disconnected. Please check your network connection.
@@ -49,7 +90,7 @@ function WebSocketDataBoundary({
     )
   }
 
-  if (state.status === 'disconnected') {
+  if (!isConnected) {
     return (
       <YStack flex={1} alignItems="center" justifyContent="center" padding="$3" gap="$2">
         <AlertCircle size="$5" color="$red9" />
