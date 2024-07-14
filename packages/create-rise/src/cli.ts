c679d11e9f1abc2eba43e0d1747e32537ba79b50
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-14 13:42:42 +0530

    wip

--- packages/create-rise/src/cli.ts
+++ packages/create-rise/src/cli.ts
@@ -4,9 +4,15 @@ import path from 'node:path'
 
 import inquirer from 'inquirer'
 
+import { emptyDir, formatTargetDir, isEmpty, write } from './utils'
+
 const cwd = process.cwd()
 
 const defaultProjectName = 'rise-project'
+
+export const renameFiles: Record<string, string> = {
+  _gitignore: '.gitignore',
+}
 
 async function prompt() {
   let targetDir = ''
@@ -51,10 +57,6 @@ async function prompt() {
     fs.mkdirSync(root, { recursive: true })
   }
 
-  const renameFiles: Record<string, string | undefined> = {
-    _gitignore: '.gitignore',
-  }
-
   const template = 'template-react-navigation'
   const templateDir = path.join(__dirname, `../${template}`)
 
@@ -62,11 +64,11 @@ async function prompt() {
 
   pkg.name = targetDir === '.' ? path.basename(path.resolve()) : targetDir
 
-  write('package.json', JSON.stringify(pkg, null, 2) + '\n')
+  write(root, templateDir, 'package.json', JSON.stringify(pkg, null, 2) + '\n')
 
   const files = fs.readdirSync(templateDir)
   for (const file of files.filter((f) => f !== 'package.json')) {
-    write(file)
+    write(root, templateDir, file)
   }
 
   console.log('Done')
@@ -74,57 +76,6 @@ async function prompt() {
   if (projectName !== '.') console.info('cd', projectName)
   console.info('npm install')
   console.info('npm start')
-
-  function write(file: string, content?: string) {
-    const targetPath = path.join(root, renameFiles[file] ?? file)
-    if (content) {
-      fs.writeFileSync(targetPath, content)
-    } else {
-      copy(path.join(templateDir, file), targetPath)
-    }
-  }
-
-  function copy(src: string, dest: string) {
-    const stat = fs.statSync(src)
-    if (stat.isDirectory()) {
-      copyDir(src, dest)
-    } else {
-      fs.copyFileSync(src, dest)
-    }
-  }
-
-  function copyDir(srcDir: string, destDir: string) {
-    fs.mkdirSync(destDir, { recursive: true })
-    for (const file of fs.readdirSync(srcDir)) {
-      const srcFile = path.resolve(srcDir, file)
-      const destFile = path.resolve(destDir, file)
-      copy(srcFile, destFile)
-    }
-  }
-
-  function isEmpty(path: string) {
-    try {
-      const files = fs.readdirSync(path)
-      return files.length === 0 || (files.length === 1 && files[0] === '.git')
-    } catch (e) {
-      return true
-    }
-  }
-
-  function emptyDir(dir: string) {
-    if (!fs.existsSync(dir)) {
-      return
-    }
-    for (const file of fs.readdirSync(dir)) {
-      if (file === '.git') {
-        continue
-      }
-      fs.rmSync(path.resolve(dir, file), { recursive: true, force: true })
-    }
-  }
-  function formatTargetDir(targetDir: string | undefined) {
-    return targetDir?.trim().replace(/\/+$/g, '')
-  }
 }
 
 prompt().catch((e) => {
