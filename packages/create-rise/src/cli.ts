a325468f9267440350ae567d1c169a4fa23188c6
Mike <grabbou@gmail.com>
2024-07-17 13:35:45 +0200

    chore: tweaks

--- packages/create-rise/src/cli.ts
+++ packages/create-rise/src/cli.ts
@@ -3,12 +3,13 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
+import dedent from 'dedent'
 import inquirer from 'inquirer'
-import { $, cd, fs, spinner } from 'zx'
+import { $, cd, chalk, fs, spinner } from 'zx'
 
 import { downloadAndExtractTemplate, formatTargetDir, isNodeError } from './utils.js'
 
-async function prompt() {
+async function createRise() {
   const { projectName } = await inquirer.prompt([
     {
       type: 'input',
@@ -78,6 +79,7 @@ async function prompt() {
   cd(root)
 
   // tbd: offer an option to choose the package manager via options
+  // maybe this is something we can share with the Community CLI?
   await spinner(`Installing dependencies in ${projectName}`, () => $`npm install`)
 
   console.log(
@@ -91,9 +93,13 @@ async function copyAdditionalTemplateFiles(root: string) {
   await $`cp ${path.join(source, '_gitignore')} ${path.join(root, '.gitignore')}`
 }
 
-// tbd: error handling
-// tbd: options/description, --help
-prompt().catch((e) => {
-  console.log(e)
+createRise().catch((e) => {
+  console.log(dedent`
+    ${chalk.red('🚨 There was an error setting up new Rise project.')}
+    ${chalk.gray(e.stack)}
+
+    If you believe this is a bug in Rise CLI, please open a new issue here:
+    ${chalk.underline('https://github.com/rise-tools/rise-tools/issues/new')}
+  `)
   process.exit(1)
 })
