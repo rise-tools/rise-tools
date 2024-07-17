edb52591e22675750b6e7076f69cb3b5eca5e174
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-18 02:37:01 +0530

    wip

--- example/demo/src/index.ts
+++ example/demo/src/index.ts
@@ -8,20 +8,18 @@ import { models as controls } from './ui-controls/ui'
 
 const models = { ...inventory, ...controls, ...delivery }
 
-
 const port = Number(process.env.PORT || '3005')
 
 createServer(models, {
-      port,
-    // ws: true,
+  port,
+  // ws: true,
 }).then((server) => {
-    console.log(server.appConnectionURL)
-    server.printQR()
+  console.log(server.appConnectionURL)
+  server.printQR()
 })
 
-
 declare module '@rise-tools/kit-react-navigation/server' {
-  type ModelKeys = keyof typeof models
-
-  export interface NavigatePath extends Record<ModelKeys, string> {}
+  export interface Navigate {
+    path: keyof typeof models
+  }
 }
--- packages/kit-react-navigation/src/server.ts
+++ packages/kit-react-navigation/src/server.ts
@@ -6,12 +6,14 @@ export type ReactNavigationActions = ActionsDefinition<
   [ReturnType<typeof navigate>, ReturnType<typeof goBack>]
 >
 
-export interface NavigatePath {}
+type PathType<T> = 'path' extends keyof T ? T['path'] : string
+
+export interface Navigate {}
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type AnyString = string & {}
 
-export const navigate = (path: keyof NavigatePath | AnyString) =>
+export const navigate = (path: PathType<Navigate> | AnyString) =>
   action('rise-tools/kit-react-navigation/navigate', { path })
 
 export const goBack = () => action('rise-tools/kit-react-navigation/goBack')
@@ -19,3 +21,5 @@ export const goBack = () => action('rise-tools/kit-react-navigation/goBack')
 export const StackScreen = createComponentDefinition<typeof Screen>(
   'rise-tools/kit-react-navigation/StackScreen'
 )
+
+navigate('')
