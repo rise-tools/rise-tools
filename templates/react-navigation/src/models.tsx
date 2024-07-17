0a39d6db7d05244ec85ebf03fb048bb4c4a9b095
Mike <grabbou@gmail.com>
2024-07-17 11:23:24 +0200

    chore: improve template

--- templates/react-navigation/src/models.tsx
+++ templates/react-navigation/src/models.tsx
@@ -1,8 +1,9 @@
-import { navigate } from '@rise-tools/kit-react-navigation/server'
-import { Button, H1, YStack } from '@rise-tools/kitchen-sink/server'
+import { goBack, navigate } from '@rise-tools/kit-react-navigation/server'
+import { Button, H1, Text, YStack } from '@rise-tools/kitchen-sink/server'
 
 export const models = {
   '': UI,
+  info: Info,
 }
 
 export function UI() {
@@ -10,6 +11,16 @@ export function UI() {
     <YStack gap="$4" backgroundColor="$background">
       <H1>Welcome to React Navigation template</H1>
       <Button onPress={navigate('info')}>Click here to navigate!</Button>
+    </YStack>
+  )
+}
+
+function Info() {
+  return (
+    <YStack>
+      <H1>Hello World!</H1>
+      <Text>You just navigated to a server-defined screen. How cool is that?</Text>
+      <Button onPress={goBack()}>Go back!</Button>
     </YStack>
   )
 }
