ed3eb2a3f7c1f47cd71197aea96d0f9214d845ca
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-20 01:00:30 +0530

    wip

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -57,11 +57,3 @@ export type InferModel<T, Prefix extends string = ''> = T extends object
     }[keyof T]
   : never
 
-// https://github.com/expo/expo/blob/4cb25674dfe73a7729ca021ceb25363d9a8e707b/packages/expo-router/src/typed-routes/types.ts#L76C1-L82C12
-export type DynamicTemplateToHrefString<Path> = Path extends `${infer PartA}/${infer PartB}`
-  ? // If the current segment (PartA) is dynamic, allow any string. This loop again with the next segment (PartB)
-    `${PartA extends `[${string}]` ? string : PartA}/${DynamicTemplateToHrefString<PartB>}`
-  : // Path is the last segment.
-    Path extends `[${string}]`
-    ? string
-    : Path
