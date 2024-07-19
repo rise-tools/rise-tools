1702fa9eba591130cff5fd1d0b90ae5c8c602a6b
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-20 00:09:56 +0530

    wip

--- example/demo/src/index.ts
+++ example/demo/src/index.ts
@@ -1,4 +1,4 @@
-import { createWSServer } from '@rise-tools/server'
+import { createWSServer, InferModel } from '@rise-tools/server'
 
 import { models as delivery } from './delivery/ui.js'
 import { models as inventory } from './inventory/ui.js'
@@ -13,6 +13,6 @@ createWSServer(models, port)
 import '@rise-tools/kit-react-navigation/server'
 declare module '@rise-tools/kit-react-navigation/server' {
   interface Navigate {
-    screens: Record<keyof typeof models, void>
+    screens: Record<InferModel<typeof models>, void>
   }
 }
--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -33,3 +33,35 @@ export type AnyModels =
   | Record<string, ValueModel<any> | LookupModel<any>>
   | ValueModel<any>
   | LookupModel<any>
+
+type LookupModelPathOf<M, Prefix extends string = ''> =
+  M extends ValueModel<any>
+    ? Prefix
+    : M extends LookupModel<infer LM>
+      ? LookupModelPathOf<LM, `${Prefix}`>
+      : M extends object
+        ? InferModel<M, `${Prefix}/[id]/`>
+        : never
+
+type ExcludeSymbol<K> = K extends symbol ? never : K
+
+export type InferModel<T, Prefix extends string = ''> = T extends object
+  ? {
+      [ModelKey in keyof T]: T[ModelKey] extends ValueModel<any>
+        ? `${Prefix}${ExcludeSymbol<ModelKey>}`
+        : T[ModelKey] extends LookupModel<infer M>
+          ? LookupModelPathOf<M, `${Prefix}${ExcludeSymbol<ModelKey>}`>
+          : T[ModelKey] extends object
+            ? InferModel<T[ModelKey], `${Prefix}${ExcludeSymbol<ModelKey>}/`>
+            : never
+    }[keyof T]
+  : never
+
+// https://github.com/expo/expo/blob/4cb25674dfe73a7729ca021ceb25363d9a8e707b/packages/expo-router/src/typed-routes/types.ts#L76C1-L82C12
+export type DynamicTemplateToHrefString<Path> = Path extends `${infer PartA}/${infer PartB}`
+  ? // If the current segment (PartA) is dynamic, allow any string. This loop again with the next segment (PartB)
+    `${PartA extends `[${string}]` ? string : PartA}/${DynamicTemplateToHrefString<PartB>}`
+  : // Path is the last segment.
+    Path extends `[${string}]`
+    ? string
+    : Path
