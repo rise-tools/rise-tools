16020e79996d0f9747a6825fbe54d11da3823b46
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-21 23:25:21 +0530

    feat: tunnel fallback UI (#183)

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,35 +1,40 @@
 import {
   clearTerminal,
+  debug,
   generateQRCode,
   getConnectionURL,
   getHost,
   highlight,
   link,
   logo,
+  safePromise,
   startTunnel,
 } from '@rise-tools/cli'
 import dedent from 'dedent'
 
 export async function printInstructions({ protocol, port }: { protocol: string; port: number }) {
   const host = (await getHost()) || 'localhost'
+
+  const [tunnelHost, tunnelError] = await safePromise(startTunnel(port))
+
   const localUrl = `${protocol}://${host}:${port}`
 
-  const remoteUrl = `${protocol}://${await startTunnel(port)}`
+  const remoteUrl = `${protocol}://${tunnelHost}`
 
   clearTerminal()
 
   console.log(logo())
 
-  const deepLink = await getConnectionURL(remoteUrl)
+  const deepLink = await getConnectionURL(remoteUrl ?? localUrl)
 
   console.log(dedent`
     Listening on ${highlight(localUrl)}
-    Access anywhere on ${highlight(remoteUrl)}  
+    Access anywhere on ${tunnelError ? debug('Tunnel available. ' + tunnelError.message) : highlight(remoteUrl)}
 
     To preview your app in the Rise Playground, scan the QR code:
     ${await generateQRCode(deepLink)}
 
     Or open the following link on your device:
-    ${link(deepLink)}  
+    ${link(deepLink)}
   `)
 }
