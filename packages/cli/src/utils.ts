602bf79a2c53ddce0bd8d06a0eea8105639c1b59
Mike <grabbou@gmail.com>
2024-07-21 13:26:19 +0200

    feat: automatically expose connection via Rise Proxy (#181)
    Given time constraints, this is much simpler and smaller implementation
    of original Tunnel idea we had in mind.
    
    Key highlights:
    - DigitalOcean VPS running [Sish](https://github.com/antoniomika/sish)
    via Docker Compose (I took me a while to figure this out). We're on $6
    plan a month. We will see how long it lasts.
    - We use [SSH tunneling](https://www.ssh.com/academy/ssh/tunneling)
    - We use `*.proxy.rise.tools` subdomain
    
    Benefits:
    - No additional tools required. Just SSH client.
    - No cloud service involved. Less things to maintain. We will eventually
    need it (more on it later).
    
    How this works:
    - Project is assigned its unique ID (via `uuid.v4()`) 
    - Project ID is stored in `.rise/projectId` file
    - Tunnel will be open on each server start automatically, with 5s
    timeout
    - Hostname is fixed to be <projectId>.proxy.rise.tools.
    
    Known limitations:
    - URL must be unique, so we use rather ugly projectId.proxy.rise.tools.
    It's not that big of a deal, since you scan QR Code or copy the deep
    link
    - Anyone can start the tunnel knowing your projectId. It means that it's
    not 100% secure (although brute force guessing your projectId is only
    theoretically possible). However, if you share your connection with
    network, it may become more likely. **That's why tunnelling should only
    be used for development**
    
    [All these two limitations can be fixed
    easily](https://github.com/antoniomika/sish/issues/313) - I opened up an
    issue in the project we're using.
    
    Once this is implemented, we will be able to:
    - use nicer names for projects, e.g. "fancy-project"
    - allow users to set custom names
    
    **Preview:**
    <img width="937" alt="Screenshot 2024-07-21 at 02 03 32"
    src="https://github.com/user-attachments/assets/474d21c5-be9c-413d-84cb-4363dc96b35b">
    
    TBD:
    - Better UX if tunnel fails and while tunnel is loading (and it's been
    more than 2s already)
    - Warn if no `''` model is present. QR Code points to index by default
    and this fails otherwise. This should be configurable.
    - Provide default screen for example. When users add it, they will loose
    default Rise Playground screen with list of available examples. We
    should print it there instead.
    - ONE DAY: maybe that default Playground screen can be Rise too!
    - SSL

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,7 +1,12 @@
+import { spawn } from 'node:child_process'
+import path from 'node:path'
+import { setTimeout } from 'node:timers/promises'
+
 import bs58 from 'bs58'
 import findUp from 'find-up'
 import internalIp from 'internal-ip'
 import qr from 'qrcode-terminal'
+import * as uuid from 'uuid'
 // @ts-ignore
 import { fs } from 'zx'
 
@@ -37,4 +42,58 @@ export async function getConnectionURL(host: string) {
     )
   )
   return `rise://connect?connectInfo=${connectionInfo}`
+}
+
+export function clearTerminal() {
+  process.stdout.write('\x1Bc')
+}
+
+async function getProjectKey(): Promise<string> {
+  const configDir = path.join(process.cwd(), '.rise')
+  const configPath = path.join(configDir, 'projectKey')
+
+  await fs.mkdirp(configDir)
+
+  if (!(await fs.exists(configPath))) {
+    const projectKey = uuid.v4()
+    await fs.writeFile(configPath, projectKey, { encoding: 'utf-8' })
+    return projectKey
+  }
+  return await fs.readFile(configPath, { encoding: 'utf-8' })
+}
+
+export async function startTunnel(port: number) {
+  const projectKey = await getProjectKey()
+
+  const session = spawn(
+    'ssh',
+    [
+      '-o',
+      'StrictHostKeyChecking no',
+      '-p',
+      '2222',
+      '-R',
+      `${projectKey}:80:localhost:${port}`,
+      'proxy.rise.tools',
+    ],
+    { stdio: ['ignore', 'pipe', 'pipe'] }
+  )
+
+  const timeoutPromise = setTimeout(5000).then(() => {
+    throw new Error(
+      'Timeout: Did not establish succesful connection with Rise Proxy. Please try again.'
+    )
+  })
+
+  const sessionPromise = new Promise<string>((resolve) => {
+    session.stdout.on('data', (data: Buffer) => {
+      console.log(data.toString())
+      const match = data.toString().match(/(?:http:\/\/)([^\s]+)/)
+      if (match) {
+        resolve(match[1]!)
+      }
+    })
+  })
+
+  return Promise.race([sessionPromise, timeoutPromise])
 }
