641a9af5d8a5fab98bbbdb24c87691b98717bfcd
Mike <grabbou@gmail.com>
2024-07-21 22:43:49 +0200

    feat: add rise tools to default template and use cross-env

--- templates/blank-playground/src/server.ts
+++ templates/blank-playground/src/server.ts
@@ -1,7 +1,12 @@
+import { setupRiseTools } from '@rise-tools/cli'
 import { createWSServer } from '@rise-tools/server'
 
 import { models } from './models'
 
 const port = Number(process.env.PORT || '3005')
 
-createWSServer(models, port)
+const server = createWSServer(models, port)
+
+if (process.env.NODE_ENV === 'development') {
+  setupRiseTools({ server })
+}
