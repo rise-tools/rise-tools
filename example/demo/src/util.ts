1a005a3148b4c4780afbfcedc90f89d69f28192a
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-23 15:55:42 +0530

    Update util.ts

--- example/demo/src/util.ts
+++ example/demo/src/util.ts
@@ -3,13 +3,14 @@ import path from 'path'
 
 export function isProcessRunning() {
   const file = path.join(process.cwd(), '.rise', 'process.pid')
+  const pid = process.ppid.toString()
   if (fs.existsSync(file)) {
-    const isSame = fs.readFileSync(file, { encoding: 'utf-8' }) === process.ppid.toString()
-    if (!isSame) fs.writeFileSync(file, process.ppid.toString())
-    return isSame
+    const isSameProcess = fs.readFileSync(file, { encoding: 'utf-8' }) === pid
+    if (!isSameProcess) fs.writeFileSync(file, pid)
+    return isSameProcess
   } else {
     fs.mkdirSync(path.dirname(file), { recursive: true })
-    fs.writeFileSync(file, process.ppid.toString())
+    fs.writeFileSync(file, pid)
     return false
   }
 }
