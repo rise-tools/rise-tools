9a10ec1a37bad5530bfe1882a979270fa69babf6
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2024-07-25 02:41:41 +0530

    feat: landing page (#190)
    <img width="383" alt="image"
    src="https://github.com/user-attachments/assets/33392a5d-5183-4fba-ac0a-1d1385b0a10d">
    <img width="1792" alt="image"
    src="https://github.com/user-attachments/assets/506f86b8-33da-44b5-987a-579dd7617a23">

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Link from '@docusaurus/Link'
+import type {} from '@docusaurus/theme-classic'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import CopyButton from '@theme/CodeBlock/CopyButton'
 import Layout from '@theme/Layout'
 import {
   ArrowRight,
@@ -20,13 +22,57 @@ import React, { ReactNode } from 'react'
 
 import { PlaygroundAppDownload } from '../components/PlaygroundAppDownload'
 
-function HomepageHeader() {
+function HeroSection() {
   return (
-    <header
-      className={
-        'bg-no-repeat bg-center bg-hero-image sm:bg-[length:1340px_150px] sm:h-[150px] bg-[length:1072px_120px] h-[120px] md:bg-[length:1605px_190px] md:h-[190px] lg:bg-[length:3216px_360px] lg:h-[360px] overflow-hidden'
-      }
-    ></header>
+    <section className="bg-gradient-to-br from-secondary/15 via-background/90 to-background -mt-[var(--ifm-navbar-height)]">
+      <div className="flex max-lg:text-center max-md:pt-40 max-lg:flex-col-reverse gap-6 mx-auto md:min-h-[90vh] items-center justify-center max-w-7xl px-4">
+        <div className="space-y-6 max-w-2xl">
+          <h1 className="rise-hero-text text-6xl max-md:text-4xl font-semibold font-poppins">
+            Server Defined Rendering for React Native
+          </h1>
+          <h2 className="text-gray-300 font-medium text-2xl max-md:text-xl">
+            Any Framework. Any Server. Ready Today.
+          </h2>
+
+          <div className="flex items-center gap-4 max-lg:justify-center ">
+            <Link
+              className="rise-button shrink-0 hover:rise-button-outline text-sm"
+              to="/docs/getting-started"
+            >
+              Get Started
+            </Link>
+            <Link
+              className="rise-button-outlined transition-all flex items-center gap-2 shrink-0  text-sm"
+              to="/docs/intro"
+            >
+              <div>Introduction</div>
+              <ArrowRight size={20} />
+            </Link>
+          </div>
+
+          <code className="px-3 py-1 inline-flex items-center gap-4">
+            npm create rise@latest
+            <div className="tooltip">
+              <CopyButton
+                code="npm create rise@latest"
+                className="relative flex items-center justify-center border border-solid border-transparent hover:border-white/40 rounded-md p-2 hover:bg-white/10 bg-white/5 transition-all opacity-70 hover:opacity-100"
+              />
+              <span className="max-sm:hidden tooltip-text rise-button w-40 text-sm font-inter -ml-20 h-8 font-medium">
+                Copy to clipboard
+              </span>
+            </div>
+          </code>
+        </div>
+        <div className="flex items-center justify-center relative">
+          <img src="/img/logo.svg" height={250} />
+          <img
+            src="/img/logo.svg"
+            height={250}
+            className="blur-2xl opacity-70 absolute animate-[spin_2s_linear_infinite]"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
@@ -34,111 +80,111 @@ const tools = [
   {
     name: 'Rise React',
     url: '/docs/core',
-    description: 'lolllll',
+    description:
+      'A core tool for building React applications with advanced features and optimizations.',
     icon: Atom,
+    color: '#FFD700',
   },
   {
     name: 'JS Server',
     url: '/docs/server-js',
-    description: 'lolllll',
+    description: 'Server-side JavaScript functionalities and configurations.',
     icon: Server,
+    color: '#1E90FF',
   },
   {
     name: 'Playground',
     url: '/docs/playground',
-    description: 'lolllll',
+    description: 'Interactive environment for testing and experimenting with code.',
     icon: SmartphoneCharging,
+    color: '#32CD32',
   },
   {
     name: 'Server API',
     url: '/docs/server-spec',
-    description: 'lolllll',
+    description: 'API specifications and documentation for server-side interactions.',
     icon: Braces,
+    color: '#FF6347',
   },
   {
     name: 'Tamagui',
     url: '/docs/kit/tamagui',
-    description: 'lolllll',
+    description: 'Design system and UI kit for rapid prototyping and development.',
     icon: Joystick,
+    color: '#FF69B4',
   },
   {
     name: 'Toast',
     url: '/docs/kit/tamagui-toast',
-    description: 'lolllll',
+    description: 'Toast notifications for user feedback and alerts.',
     icon: PartyPopper,
+    color: '#FFC0CB',
   },
   {
     name: 'Linking',
     url: '/docs/kit/linking',
-    description: 'lolllll',
+    description: 'Linking and navigation utilities for enhanced user experience.',
     icon: LinkIcon,
+    color: '#8A2BE2',
   },
   {
     name: 'Haptics',
     url: '/docs/kit/haptics',
-    description: 'lolllll',
+    description: 'Haptic feedback features to enhance the tactile experience.',
     icon: Vibrate,
+    color: '#00FA9A',
   },
   {
     name: 'QR Code',
     url: '/docs/kit/qrcode',
-    description: 'lolllll',
+    description: 'QR code generation and scanning functionalities.',
     icon: QrCode,
+    color: '#FF4500',
   },
   {
     name: 'Navigation',
     url: '/docs/guides/navigation',
-    description: 'lolllll',
+    description: 'Guides and best practices for implementing navigation in applications.',
     icon: Compass,
+    color: '#6A5ACD',
   },
   {
     name: 'Forms',
     url: '/docs/kit/forms',
-    description: 'lolllll',
+    description: 'Components and utilities for form handling and validation.',
     icon: ClipboardPen,
+    color: '#20B2AA',
   },
   {
     name: 'Icons',
     url: '/docs/kit/lucide-icons',
-    description: 'lolllll',
+    description: 'Collection of icons for use in your projects.',
     icon: Drama,
+    color: '#DC143C',
   },
 ]
 
-function Section({ children, alt = false }: { children: ReactNode; alt?: boolean }) {
-  return (
-    <section className={`${alt ? 'bg-black' : ''}`}>
-      <div className="lg:container w-100  md:px-8 px-4 md:py-12 py-8 space-y-10 mx-auto">
-        {children}
-      </div>
-    </section>
-  )
-}
-
-function SectionContent({
+function Section({
+  children,
   title,
-  text,
-  graphic,
-  footer,
+  subTitle,
+  className,
 }: {
-  title: string
-  text: string
-  graphic: ReactNode
-  footer?: ReactNode
+  children: ReactNode
+  title?: string
+  subTitle?: string
+  className?: string
 }) {
   return (
-    <div className="space-y-8">
-      <h2 className="flex center rise-gradient-text font-extrabold lg:text-7xl md:text-5xl text-4xl text-center">
+    <section className={'py-10 bg-background text-center ' + className}>
+      <div className="rise-gradient-text lg:text-5xl text-3xl font-semibold font-poppins !leading-normal max-w-4xl text-center">
         {title}
-      </h2>
-      <p>{text}</p>
-      <div>
-        <div className={'flex flex-col center drop-shadow-md rounded-2xl overflow-hidden'}>
-          {graphic}
-        </div>
       </div>
-      {footer && <div>{footer}</div>}
-    </div>
+      {subTitle && (
+        <p className="text-center font-medium md:text-xl text-lg text-gray-300">{subTitle}</p>
+      )}
+      <div className="mx-auto md:px-8 px-4 md:py-12 py-8 space-y-10 max-w-7xl">{children}</div>
+    </section>
   )
 }
 
@@ -149,70 +195,53 @@ export default function Home() {
       title={`${siteConfig.tagline}`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
+      <HeroSection />
+
+      <Section title="Meet The Tools">
+        <div className={'grid gap-4 md:gap-10 sm:grid-cols-2 md:grid-cols-4'}>
+          {tools.map((tool) => {
+            return (
+              <a
+                className="hover:no-underline text-white md:space-y-3 p-4 rounded-lg bg-background/40 max-md:flex items-start gap-4 text-left "
+                key={tool.name}
+                href={tool.url}
+              >
+                <div
+                  className="border-solid p-2 inline-flex items-center justify-center rounded-md max-sm:mt-2"
+                  style={{
+                    borderColor: tool.color,
+                    color: tool.color,
+                    backgroundColor: `${tool.color}50`,
+                  }}
+                >
+                  <tool.icon size={20} />
+                </div>
+                <div>
+                  <div className="font-poppins text-gray-200 font-semibold text-lg leading-loose">
+                    {tool.name}
+                  </div>
+                  <div className="font-medium text-gray-500 leading-5 text-wrap break-words">
+                    {tool.description}
+                  </div>
+                </div>
+              </a>
+            )
+          })}
+        </div>
+      </Section>
 
       <main className={'bg-gradient-to-tl from-background from-30% to-black'}>
-        <div className={'flex center p-5 gap-5 md:mt-10 mt-5'}>
-          <Link className="rise-button shrink-0" to="/docs/getting-started">
-            Get Started
-          </Link>
-          <Link className="rise-button-outlined flex items-center gap-2 shrink-0" to="/docs/intro">
-            <div>Introduction</div>
-            <ArrowRight className="w-6 h-6 " />
-          </Link>
-        </div>
-        <Section>
-          <SectionContent
-            title="Forms and UI, defined on your server"
-            text=""
-            graphic={<img src="/img/forms-ui.png" />}
-          />
+        <Section title="Forms and UI" subTitle="Defined on your server" className=" bg-black">
+          <img src="/img/forms-ui.png" className="md:w-[60vw]" />
         </Section>
-        <section className="bg-white px-10 py-8">
-          <h2 className="lg:text-6xl md:text-4xl text-3xl font-extrabold text-black text-center">
-            Any Framework.
-          </h2>
-          <h2 className="lg:text-6xl md:text-4xl text-3xl font-extrabold text-black text-center">
-            Any Server.
-          </h2>
-          <h2 className="lg:text-6xl md:text-4xl text-3xl font-extrabold text-black text-center">
-            Ready Today.
-          </h2>
-        </section>
 
-        <Section alt>
-          <SectionContent
-            title="Local Actions. Remote Events. Dynamic Routes."
-            text=""
-            graphic={<img src="/img/actions-events-routes.png" />}
-          />
+        <Section title="Playground App" subTitle="Jump-Start Your Dev">
+          <PlaygroundAppDownload />
+          <img src="/img/playground-app.png" className="mx-auto" />
         </Section>
-        <Section>
-          <SectionContent
-            title="Playground App: Jump-Start Your Dev"
-            text=""
-            graphic={<img src="/img/playground-app.png" />}
-            footer={<PlaygroundAppDownload />}
-          />
-        </Section>
-        <Section alt>
-          <h2 className={'rise-gradient-text lg:text-6xl md:text-4xl text-3xl font-extrabold'}>
-            Meet The Tools
-          </h2>
-          <div className={'grid gap-10 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6'}>
-            {tools.map((tool) => {
-              return (
-                <a
-                  className={'flex flex-col items-center  hover:no-underline text-white'}
-                  key={tool.name}
-                  href={tool.url}
-                >
-                  <h4>{tool.name}</h4>
-                  <tool.icon size={64} />
-                </a>
-              )
-            })}
-          </div>
+
+        <Section title="Local Actions. Remote Events. Dynamic Routes." className="bg-black">
+          <img src="/img/actions-events-routes.png" />
         </Section>
       </main>
     </Layout>
