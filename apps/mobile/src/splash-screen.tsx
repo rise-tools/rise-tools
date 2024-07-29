29e3ac44ca5a0fcdaf6db384aad31d9175bf30eb
TheKohan <kohan142011@gmail.com>
2024-07-29 16:11:05 +0200

    feat: transition bg opacity

--- apps/mobile/src/splash-screen.tsx
+++ apps/mobile/src/splash-screen.tsx
@@ -11,12 +11,14 @@ import Animated, {
 Splash.preventAutoHideAsync()
 
 const AnimatedImage = Animated.createAnimatedComponent(Image)
+const AnimatedView = Animated.createAnimatedComponent(View)
 
 export function SplashScreen({ children, loaded }: { children: React.ReactNode; loaded: boolean }) {
   const [isAppReady, setIsAppReady] = useState(false)
   const opacity = useSharedValue(0)
+  const bgOpacity = useSharedValue(1)
   const scale = useSharedValue(0.5)
-  const rotation = useSharedValue(0)
+  const rotation = useSharedValue(315)
 
   useEffect(() => {
     startAnimation()
@@ -24,15 +26,17 @@ export function SplashScreen({ children, loaded }: { children: React.ReactNode; 
 
   const startAnimation = async () => {
     await Splash.hideAsync()
-    opacity.value = withTiming(1, { duration: 2000 })
-    rotation.value = withTiming(360, { duration: 2000 })
-    scale.value = withTiming(1, { duration: 2000 }, () => {
-      runOnJS(onAnimationComplete)()
+    opacity.value = withTiming(1, { duration: 500 })
+    rotation.value = withTiming(360, { duration: 500 })
+    scale.value = withTiming(1, { duration: 500 }, () => {
+      bgOpacity.value = withTiming(0, { duration: 500 }, () => {
+        runOnJS(onAnimationComplete)()
+      })
     })
   }
 
   const onAnimationComplete = async () => {
-    setTimeout(() => setIsAppReady(true), 1000)
+    setTimeout(() => setIsAppReady(true), 500)
   }
 
   const animatedStyles = useAnimatedStyle(() => {
@@ -41,25 +45,31 @@ export function SplashScreen({ children, loaded }: { children: React.ReactNode; 
       transform: [{ scale: scale.value }, { rotate: rotation.value + 'deg' }],
     }
   })
+  const animatedBgStyles = useAnimatedStyle(() => {
+    return {
+      opacity: bgOpacity.value,
+    }
+  })
 
-  if (!isAppReady || !loaded) {
-    return (
-      <View style={styles.container}>
-        <AnimatedImage
-          source={require('../assets/RiseMainIcon.png')}
-          style={[styles.logo, animatedStyles]}
-          resizeMode="contain"
-        />
-      </View>
-    )
-  }
-
-  return <>{children}</>
+  return (
+    <View style={{ flex: 1 }}>
+      {children}
+      {(!isAppReady || !loaded) && (
+        <AnimatedView style={[styles.container, animatedBgStyles]}>
+          <AnimatedImage
+            source={require('../assets/RiseMainIcon.png')}
+            style={[styles.logo, animatedStyles]}
+            resizeMode="contain"
+          />
+        </AnimatedView>
+      )}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#260162',
