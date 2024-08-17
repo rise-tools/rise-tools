d1bfec53aff3d698874c13557e7a5e0f2f7fdb3b
Mike <grabbou@gmail.com>
2024-08-17 19:47:58 +0200

    fix: rendering Rise JSX from React Native / Metro context
    Historically, we had this empty file so React Native can build (events had some references to node:crypto).
    
    Now, it's not needed. We're actually rendering some Rise JSX inside React Native application (for fallback UI) and this is breaking it.

--- packages/react/src/events.native.ts
+++
@@ -1,4 +0,0 @@
-// tbd: move events.ts to a separate package so React Native can build
-// without failing on Node dependency
-//
-// Easiest until experimental_packageExports is turned on by default
