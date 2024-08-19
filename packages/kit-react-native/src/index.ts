078bc7c1dd3a85f88f03da0d09802055191398f3
Mike <grabbou@gmail.com>
2024-08-19 18:17:38 +0200

    add preliminary rn components

---
+++ packages/kit-react-native/src/index.ts
@@ -0,0 +1,59 @@
+import { ComponentRegistry } from '@rise-tools/react'
+import * as RN from 'react-native'
+
+export const ReactNativeComponents: ComponentRegistry = {
+  'rise-tools/kit-react-native/ActivityIndicator': {
+    component: RN.ActivityIndicator,
+  },
+  'rise-tools/kit-react-native/Button': {
+    component: RN.Button,
+  },
+  'rise-tools/kit-react-native/Modal': {
+    component: RN.Modal,
+  },
+  'rise-tools/kit-react-native/KeyboardAvoidingView': {
+    component: RN.KeyboardAvoidingView,
+  },
+  'rise-tools/kit-react-native/Pressable': {
+    component: RN.Pressable,
+  },
+  'rise-tools/kit-react-native/RefreshControl': {
+    component: RN.RefreshControl,
+  },
+  'rise-tools/kit-react-native/ScrollView': {
+    component: RN.ScrollView,
+  },
+  'rise-tools/kit-react-native/SectionList': {
+    component: RN.SectionList,
+  },
+  'rise-tools/kit-react-native/StatusBar': {
+    component: RN.StatusBar,
+  },
+  'rise-tools/kit-react-native/Switch': {
+    component: RN.Switch,
+  },
+  'rise-tools/kit-react-native/Text': {
+    component: RN.Text,
+  },
+  'rise-tools/kit-react-native/TextInput': {
+    component: RN.TextInput,
+  },
+  'rise-tools/kit-react-native/TouchableHighlight': {
+    component: RN.TouchableHighlight,
+  },
+  'rise-tools/kit-react-native/TouchableOpacity': {
+    component: RN.TouchableOpacity,
+  },
+  'rise-tools/kit-react-native/TouchableNativeFeedback': {
+    component: RN.TouchableNativeFeedback,
+  },
+  'rise-tools/kit-react-native/TouchableWithoutFeedback': {
+    component: RN.TouchableWithoutFeedback,
+  },
+  'rise-tools/kit-react-native/View': {
+    component: RN.View,
+  },
+  'rise-tools/kit-react-native/VirtualizedList': {
+    component: RN.VirtualizedList,
+  },
+}
