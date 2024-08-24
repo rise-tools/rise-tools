f5dd64d7728e3ba45fdee0334a76a70869e9d136
Eric Vicenti <eric@vicenti.net>
2024-08-24 22:12:13 +0200

    fix: state mutation setter allows silent updates

--- packages/server/src/__tests__/model-state.test.ts
+++ packages/server/src/__tests__/model-state.test.ts
@@ -15,6 +15,26 @@ describe('model-state', () => {
     setS((s) => s + 1)
     expect(s.get()).toBe(1)
   })
+  test('mutation setter with no update', () => {
+    const handler = vi.fn()
+    const [s, setS] = state(0)
+    const release = s.subscribe(handler)
+    expect(handler).toBeCalledTimes(1)
+    expect(handler).toHaveBeenLastCalledWith(0)
+    expect(s.get()).toBe(0)
+    setS((s) => s)
+    expect(handler).toBeCalledTimes(1)
+    setS((s) => s + 1)
+    expect(handler).toBeCalledTimes(2)
+    expect(handler).toHaveBeenLastCalledWith(1)
+    release()
+    setS((s) => s + 1)
+    expect(s.get()).toBe(2)
+    // this is the main thing we are checking in this test:
+    expect(handler).toBeCalledTimes(2)
+    // if the mutation setter is returns the input value, the subscriber should not be called
+    expect(handler).toHaveBeenLastCalledWith(1)
+  })
   test('subscribe', () => {
     const handler = vi.fn()
     const [s, setS] = state(0)
